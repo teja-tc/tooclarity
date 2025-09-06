19923cb54c8120adf00336dd7e160c1cc42147b9
karan-tooclarity <tooclaritytc06@gmail.com>
2025-09-06 16:22:08 +0530

    updated backend validators for L2 and updated institution model for coaching center , study halls and study abroad

--- backend/middleware/validators.js
+++ backend/middleware/validators.js
@@ -45,7 +45,7 @@ exports.validateLogin = [
 exports.validateL1Creation = [
     body('instituteName').notEmpty().withMessage('Institution name is required').trim().isLength({ max: 150 }).escape(),
     body('instituteType').isIn([
-        'Kindergarten/childcare center', 'School', 'Intermediate college(K12)', 'Under Graduation/Post Graduation', 'Coaching centers', 'Study halls', 'Study Abroad'
+        'Kindergarten/childcare center', "School's", 'Intermediate college(K12)', 'Under Graduation/Post Graduation', 'Coaching centers',"Tution Center's", 'Study Halls', 'Study Abroad'
     ]).withMessage('A valid institute type is required.'),
     // body('establishmentDate').optional({ checkFalsy: true }).isISO8601().toDate().withMessage('Establishment date must be a valid date.'),
     body('approvedBy').optional({ checkFalsy: true }).trim().isLength({ max: 100 }).escape(),
@@ -100,11 +100,13 @@ exports.validateL2Update = async (req, res, next) => {
         let validationChain = [];
         switch (institution.instituteType) {
             case 'Kindergarten/childcare center': validationChain = kindergartenL2Rules; break;
-            case 'School':
+            case "School's": return next(); break;
             case 'Intermediate college(K12)': validationChain = schoolAndIntermediateL2Rules; break;
             case 'UG / PG University': validationChain = ugPgUniversityL2Rules; break;
             case 'Study Abroad': return next();
             case 'Coaching centers': return next();
+            case "Tution Center's" : return next();
+            case 'Study Halls': return next();
             default:
                 logger.error({ userId: req.userId, instituteType: institution.instituteType }, 'Unsupported institution type for L2 update.');
                 return res.status(400).json({ status: 'fail', message: 'Unsupported institution type for L2 update.' });
