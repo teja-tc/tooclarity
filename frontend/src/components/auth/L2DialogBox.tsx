0a806d316c502a1fa7de24e3407b5ccc97ed77bb
karan-tooclarity <tooclaritytc06@gmail.com>
2025-10-12 20:44:15 +0530

    fixed course updating bug in l2DialogBox

--- frontend/src/components/auth/L2DialogBox.tsx
+++ frontend/src/components/auth/L2DialogBox.tsx
@@ -860,7 +860,11 @@ export default function L2DialogBox({
         } else {
           await addCoursesGroupToDB({
             branchName: entry.branchName || "",
+            branchAddress: entry.branchAddress || "",
+            contactInfo: entry.contactInfo || "",
+            locationUrl: entry.locationUrl || "",
             courses: entry.courses || [],
+
           });
         }
       }
